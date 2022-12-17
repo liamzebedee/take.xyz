131a61edb37e33be823682eba5d9aa14c899a426
liamzebedee <liamzebedee@yahoo.com.au>
2022-12-17 15:08:10 +1100

    .

--- components/header.js
+++ components/header.js
@@ -24,21 +24,31 @@ import { useEnsName } from 'wagmi'
 import truncateEthAddress from 'truncate-eth-address'
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+const classNames = require('classnames');
+
+const {
+    menuLiActive,
+    menuLiLiterallyAnythingElse
+} = styles
 
 export default function Header() {
     // Manually compute the active page
     const router = useRouter()
     const isHome = router.pathname == "/"
     const isFeed = router.pathname == "/feed"
+    const isLiterallyAnythingElse = !isHome && !isFeed
 
     return <header className={styles.header}>
         <div className={styles.menu}>
             <ul>
-                <li className={isHome ? styles.menuLiActive : undefined}>
+                <li className={classNames({ [menuLiActive]: isHome })}>
                     <Link href="/">home</Link>
                 </li>
-                <li className={isFeed ? styles.menuLiActive : undefined}>
+                <li className={classNames({ [menuLiActive]: isFeed })}>
                     <Link href="/feed">feed</Link>
+                </li>
+                <li className={classNames({ [menuLiActive]: isLiterallyAnythingElse, [menuLiLiterallyAnythingElse]: true })}>
+                    <span>✴✴✴</span>
                 </li>
             </ul>
         </div>
