7fa9d23c21dbfb3d4cfa3c366396450bcd9d6f89
liamzebedee <liamzebedee@yahoo.com.au>
2022-12-18 20:28:09 +1100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  if(Component.layout) {
-    return <Component.layout>
-      <Component {...pageProps} />
-    </Component.layout>
-  }
-  return <Component {...pageProps} />
+  const Layout = Component.layout || (({ children }) => <>{children}</>)
+  return <Layout>
+    <Head>
+      <link rel="apple-touch-icon" href="apple-touch-icon.png"></link>
+    </Head>
+    <Component {...pageProps} />
+  </Layout>
 }
 
 export default MyApp
