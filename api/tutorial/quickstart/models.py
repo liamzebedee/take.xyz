d551cfd1b2fc8433a9c1ee963c6d6fb9b6d10590
liamzebedee <liamzebedee@yahoo.com.au>
2023-01-31 13:00:48 +1100

    add like

--- api/tutorial/quickstart/models.py
+++ api/tutorial/quickstart/models.py
@@ -4,6 +4,7 @@ class Take(models.Model):
     nft_id = models.IntegerField(unique=True)
     text = models.TextField()
     creator = models.ForeignKey('User', on_delete=models.CASCADE, related_name='created_takes', null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     # A Take can have many remixes, and a remix can have many takes as sources.
     remixes = models.ManyToManyField('self', through='Remix', through_fields=('source', 'mix'), blank=True)
@@ -14,6 +15,12 @@ class Take(models.Model):
 class Remix(models.Model):
     source = models.ForeignKey(Take, on_delete=models.CASCADE, related_name='remix_source')
     mix = models.ForeignKey(Take, on_delete=models.CASCADE, related_name='remix_mix')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    take = models.ForeignKey(Take, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class User(models.Model):
     address = models.CharField(max_length=42, unique=True)
